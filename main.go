6f1dfbbfe476be85e97b39b31a8962fe8ae6d4ae
endijulian <endijulian080798@gmail.com>
2022-07-21 15:41:15 +0700

    func handler

--- main.go
+++ main.go
@@ -8,13 +8,31 @@ import (
 func main() {
 
 	mux := http.NewServeMux()
+
+	aboutHandler := func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("About Page"))
+	}
+
 	mux.HandleFunc("/", homeHandler)
 	mux.HandleFunc("/hello", helloHandler)
 	mux.HandleFunc("/endi", endiHandler)
+	mux.HandleFunc("/about", aboutHandler)
+
+	mux.HandleFunc("/profile", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Profile Page"))
+	})
 
 	log.Println("Startting web on Port 8080")
 	err := http.ListenAndServe(":8080", mux)
 	log.Fatal(err)
+}
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+	w.Write([]byte("Welcome to page HOME"))
 }
 
 func helloHandler(w http.ResponseWriter, r *http.Request) {
@@ -23,8 +41,4 @@ func helloHandler(w http.ResponseWriter, r *http.Request) {
 
 func endiHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello, Nama saya Endi Julian"))
-}
-
-func homeHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Welcome to page HOME"))
 }
